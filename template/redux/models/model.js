2ea98cc71adb8a7b0026ba46f76b1cf1f98f95c3
wanghxm <wanghxm@yonyou.com>
2019-04-20 10:34:11 +0800

    model增加多级赋值

--- template/redux/models/model.js
+++ template/redux/models/model.js
@@ -11,6 +11,37 @@ export default {
                 ...state,
                 ...payload
             };
+        },
+        /**
+         * 特定层级合并赋值, 可以传一级结构也可以二级结构，也可以同时传
+         * @param {*} state 
+         * @param {*} searchParams 
+         */
+        handleParamsByKey(state, searchParams) {
+            let key = Object.keys(searchParams)
+            let value = Object.values(searchParams)
+            let params
+            let tempState = state
+            let tostring = Object.prototype.toString
+            key.forEach((v, k)=> {
+                let ckey = key[k]
+                if (tostring.call(tempState[ckey])  === '[object Object]') {
+                    params = Object.assign({}, state[ckey], {...value[k]})
+                    tempState = {
+                        ...tempState,
+                        [ckey]: params
+                    }
+                }  else {
+                    tempState = {
+                        ...tempState,
+                        [ckey]: searchParams[ckey]
+                    }
+                }
+            })
+            return {
+                ...state,
+                ...tempState
+            }
         }
     },
     async: {
